d022cd8aa09db6c265191efed8ceb40528ce8170
AshFoster <83658970+AshFoster@users.noreply.github.com>
2025-08-03 11:19:35 +0100

    Initial commit.

---
+++ src/main/java/com/thedarklegend/ultimatetexasholdem/Main.java
@@ -0,0 +1,9 @@
+package com.thedarklegend.ultimatetexasholdem;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ultimate Texas Hold'em!" );
+    }
+}
