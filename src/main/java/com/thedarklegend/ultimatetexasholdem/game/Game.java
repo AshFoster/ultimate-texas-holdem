526882f5bbbcf23c2c96ad978fd492fc89c5e310
AshFoster <83658970+AshFoster@users.noreply.github.com>
2025-08-04 15:27:18 +0100

    Add dealFlop() method and test.

--- src/main/java/com/thedarklegend/ultimatetexasholdem/game/Game.java
+++ src/main/java/com/thedarklegend/ultimatetexasholdem/game/Game.java
@@ -1,20 +1,26 @@
 package com.thedarklegend.ultimatetexasholdem.game;
 
+import com.thedarklegend.ultimatetexasholdem.model.Card;
 import com.thedarklegend.ultimatetexasholdem.model.Dealer;
 import com.thedarklegend.ultimatetexasholdem.model.Deck;
 import com.thedarklegend.ultimatetexasholdem.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game
 {
     private final Deck deck;
     private final Player player;
     private final Dealer dealer;
+    private final List<Card> communityCards;
 
     public Game()
     {
         this.deck = new Deck();
         this.player = new Player();
         this.dealer = new Dealer();
+        this.communityCards = new ArrayList<>();
     }
 
     public void start()
@@ -31,6 +37,25 @@ public class Game
         dealer.receiveCard(deck.draw());
     }
 
+    public void dealFlop()
+    {
+        burn();
+        for (int i = 0; i < 3; i++)
+        {
+            communityCards.add(deck.draw());
+        }
+    }
+
+    private void burn()
+    {
+        deck.draw();
+    }
+
+    public Deck getDeck()
+    {
+        return deck;
+    }
+
     public Player getPlayer()
     {
         return player;
@@ -39,5 +64,10 @@ public class Game
     public Dealer getDealer()
     {
         return dealer;
+    }
+
+    public List<Card> getCommunityCards()
+    {
+        return new ArrayList<>(communityCards);
     }
 }
--- src/test/java/com/thedarklegend/ultimatetexasholdem/game/GameTest.java
+++ src/test/java/com/thedarklegend/ultimatetexasholdem/game/GameTest.java
@@ -15,4 +15,15 @@ public class GameTest
        assertEquals(2, game.getPlayer().getHand().size());
        assertEquals(2, game.getDealer().getHand().size());
    }
+
+    @Test
+    void dealFlopShouldBurnOneCardAndDealThreeCommunityCards()
+    {
+        Game game = new Game();
+        game.start();
+        game.dealFlop();
+
+        assertEquals(52 - 8, game.getDeck().size());
+        assertEquals(3, game.getCommunityCards().size());
+    }
 }
